f13ce6014ac1a8b089b7453556937e70aa071d08
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-08-05 19:00:34 +0200

    feat: button variants (#10)

--- packages/react/src/button/Button.stories.ts
+++ packages/react/src/button/Button.stories.ts
@@ -1,16 +1,10 @@
-// List.stories.ts|tsx
-
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { default as Button } from "./Button";
+import { Button } from "./Button";
 
-//👇 Instead of importing ListItem, we import the stories
+const buttonLabel = "Click me";
 
 const meta: Meta<typeof Button> = {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: "Button",
   component: Button,
 };
@@ -18,21 +12,100 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
-  name: "Primary",
+export const PrimaryBold: Story = {
+  name: "Primary Bold",
   args: {
-    children: "Hello World",
+    children: buttonLabel,
     variant: "primary",
+    emphasis: "bold",
     size: "lg",
     disabled: false,
   },
 };
 
-export const Secondary: Story = {
-  name: "Secondary",
+export const PrimarySubtle: Story = {
+  name: "Primary Subtle",
   args: {
-    children: "Hello World",
+    children: buttonLabel,
+    variant: "primary",
+    emphasis: "subtle",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const PrimaryMinimal: Story = {
+  name: "Primary Minimal",
+  args: {
+    children: buttonLabel,
+    variant: "primary",
+    emphasis: "minimal",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const SecondaryBold: Story = {
+  name: "Secondary Bold",
+  args: {
+    children: buttonLabel,
     variant: "secondary",
+    emphasis: "bold",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const SecondarySubtle: Story = {
+  name: "Secondary Subtle",
+  args: {
+    children: buttonLabel,
+    variant: "secondary",
+    emphasis: "subtle",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const SecondaryMinimal: Story = {
+  name: "Secondary Minimal",
+  args: {
+    children: buttonLabel,
+    variant: "secondary",
+    emphasis: "minimal",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const DangerBold: Story = {
+  name: "Danger Bold",
+  args: {
+    children: buttonLabel,
+    variant: "danger",
+    emphasis: "bold",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const DangerSubtle: Story = {
+  name: "Danger Subtle",
+  args: {
+    children: buttonLabel,
+    variant: "danger",
+    emphasis: "subtle",
+    size: "lg",
+    disabled: false,
+  },
+};
+
+export const DangerMinimal: Story = {
+  name: "Danger Minimal",
+  args: {
+    children: buttonLabel,
+    variant: "danger",
+    emphasis: "minimal",
     size: "lg",
     disabled: false,
   },
--- packages/react/src/button/Button.tsx
+++ packages/react/src/button/Button.tsx
@@ -6,25 +6,85 @@ const buttonVariants = cva(
   "transition-all duration-200 flex gap-1 items-center font-semibold disabled:pointer-events-none",
   {
     variants: {
+      emphasis: {
+        bold: "",
+        subtle: "",
+        minimal: "",
+      },
       variant: {
-        primary:
-          "bg-primary-500 text-white hover:bg-primary-400 focus:ring-[#E4D8FD] focus-visible:outline-none focus:ring-4 disabled:bg-primary-50",
-        secondary:
-          "bg-neutral-200 text-theme-text-primary hover:bg-neutral-300 focus-visible:outline-none active:bg-neutral-400 focus:ring-4 focus:ring-[#E5E7EB] disabled:text-neutral-100",
-        transparent:
-          "bg-transparent text-theme-text-secondary hover:bg-neutral-300 hover:text-theme-text-primary focus:bg-neutral-400 focus:outline-none focus:ring-4 focus:ring-[#E5E7EB] disabled:text-theme-text-tertiary",
-        headless: "",
-        danger:
-          "bg-error-600 text-white hover:bg-error-500 focus:ring-error-50 focus:bg-error-600 focus-visible:outline-none focus:ring-4 disabled:bg-error-50",
+        primary: "",
+        secondary: "",
+        danger: "",
       },
       size: {
-        sm: "rounded-[8px] py-1 px-2",
-        lg: "rounded-[8px] py-2 px-3",
-        xl: "rounded-[8px] py-3 px-5",
+        sm: "rounded-lg py-1 px-2",
+        lg: "rounded-lg py-2 px-3",
+        xl: "rounded-lg py-3 px-5",
       },
     },
+    compoundVariants: [
+      // Primary
+      {
+        emphasis: "bold",
+        variant: "primary",
+        class:
+          "bg-primary-500 text-white hover:bg-primary-400 focus:ring-[#E4D8FD] active:bg-primary-500 focus-visible:outline-none focus:ring-4 disabled:bg-primary-50",
+      },
+      {
+        emphasis: "subtle",
+        variant: "primary",
+        class:
+          "border border-primary-400 text-primary-600 hover:bg-primary-50 active:bg-primary-100 active:ring-4 active:ring-[#E4D8FD] disabled:border-primary-100 disabled:text-primary-100",
+      },
+      {
+        emphasis: "minimal",
+        variant: "primary",
+        class:
+          "text-primary-600 hover:bg-primary-50 active:bg-primary-100 active:ring-4 active:ring-[#E4D8FD] disabled:text-primary-100",
+      },
+      // Secondary
+      {
+        emphasis: "bold",
+        variant: "secondary",
+        class:
+          "bg-neutral-200 text-theme-text-primary hover:bg-neutral-300 focus-visible:outline-none active:bg-neutral-400 focus:ring-4 focus:ring-[#E5E7EB] disabled:text-neutral-100",
+      },
+      {
+        emphasis: "subtle",
+        variant: "secondary",
+        class:
+          "border border-neutral-300 text-neutral-900 hover:bg-neutral-100 active:border-neutral-400 active:bg-neutral-300 active:ring-4 active:ring-neutral-100 disabled:border-neutral-300 disabled:text-neutral-300",
+      },
+      {
+        emphasis: "minimal",
+        variant: "secondary",
+        class:
+          "text-primary-900 hover:bg-neutral-200 active:bg-neutral-300 active:ring-4 active:ring-neutral-100 disabled:text-neutral-300",
+      },
+      // Danger
+      {
+        emphasis: "bold",
+        variant: "danger",
+        class:
+          "bg-error-600 text-white hover:bg-error-500 focus:ring-error-50 focus:bg-error-600 focus-visible:outline-none focus:ring-4 disabled:bg-error-100",
+      },
+      {
+        emphasis: "subtle",
+        variant: "danger",
+        class:
+          "border border-error-600 text-error-700 hover:bg-error-50 active:bg-error-100 active:ring-4 active:ring-error-50 disabled:border-error-100 disabled:text-error-100",
+      },
+      {
+        emphasis: "minimal",
+        variant: "danger",
+        class:
+          "text-error-700 hover:bg-error-50 active:bg-error-100 active:ring-4 active:ring-error-50 disabled:text-error-100",
+      },
+    ],
     defaultVariants: {
       size: "sm",
+      emphasis: "bold",
+      variant: "primary",
     },
   }
 );
@@ -33,24 +93,23 @@ type ButtonVariantsProps = VariantProps<typeof buttonVariants>;
 
 interface ButtonVariants
   extends ButtonHTMLAttributes<HTMLButtonElement>,
-    Omit<ButtonVariantsProps, "variant">,
-    Required<Pick<ButtonVariantsProps, "variant">> {}
+    Omit<ButtonVariantsProps, "variant" | "emphasis">,
+    Required<Pick<ButtonVariantsProps, "variant" | "emphasis">> {}
 
-function Button({
+export function Button({
   className,
   children,
   variant,
+  emphasis,
   size,
   ...rest
 }: ButtonVariants) {
   return (
     <button
-      className={cn(buttonVariants({ size, variant }), className)}
+      className={cn(buttonVariants({ size, variant, emphasis }), className)}
       {...rest}
     >
       {children}
     </button>
   );
 }
-
-export default Button;
